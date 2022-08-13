f98f641ddecb7c66545c8264127d3a79da0c91d5
waterproofpatch <waterproofpatch@gmail.com>
2022-08-12 20:22:36 -0400

    refactor

--- SslChat/Server/client.cpp
+++ SslChat/Server/client.cpp
@@ -10,25 +10,6 @@
 #include "client.hpp"
 #include "logging.hpp"
 
-void Client::start()
-{
-    LOG_INFO("Starting thread...");
-    this->t = new std::thread(&Client::run, this);
-    LOG_INFO("Started thread...");
-}
-void Client::stop()
-{
-    LOG_INFO("Joining thread...");
-    close(this->socket);
-    this->t->join();
-    LOG_INFO("Joined thread...");
-}
-void Client::run()
-{
-    LOG_INFO("Running!");
-    SSL_write(this->ssl, "Reply!", strlen("Reply!"));
-    return;
-}
 Client::Client(SSL *ssl, int socket)
 {
     LOG_INFO("Client constructing!");
@@ -37,7 +18,29 @@ Client::Client(SSL *ssl, int socket)
 }
 Client::~Client()
 {
+
     LOG_INFO("Client destructing!");
     SSL_shutdown(this->ssl);
     SSL_free(this->ssl);
+}
+void Client::start()
+{
+    LOG_INFO("Starting thread...");
+    this->t = new std::thread(&Client::run, this);
+    LOG_INFO("Started thread...");
+}
+
+void Client::stop()
+{
+    LOG_INFO("Joining thread...");
+    close(this->socket);
+    this->t->join();
+    LOG_INFO("Joined thread...");
+}
+
+void Client::run()
+{
+    LOG_INFO("Running!");
+    SSL_write(this->ssl, "Reply!", strlen("Reply!"));
+    return;
 }
