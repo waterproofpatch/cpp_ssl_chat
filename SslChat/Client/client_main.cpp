1a28e43756b58821f314b9b66958db937afcd02a
waterproofpatch <waterproofpatch@gmail.com>
2022-08-19 06:48:15 -0400

    handlemessages

--- SslChat/Client/client_main.cpp
+++ SslChat/Client/client_main.cpp
@@ -11,6 +11,7 @@
 #include <openssl/ssl.h>
 
 #include "constants.hpp"
+#include "handleMessages.hpp"
 #include "logging.hpp"
 #include "openConnection.hpp"
 #include "ssl.hpp"
@@ -37,23 +38,6 @@ void readMessages(SSL *ssl)
         LOG_INFO(fmt::format("Read [{}]", buffer));
     }
     LOG_INFO("Tearing down!");
-}
-
-int handleMessages(SSL *ssl)
-{
-    for (std::string line; std::getline(std::cin, line);)
-    {
-        std::cout << line << std::endl;
-        if (line.compare("quit") == 0)
-        {
-            LOG_INFO("Quitting.");
-            break;
-        }
-        LOG_INFO(fmt::format("Sending [{}]", line));
-        SSL_write(ssl, line.c_str(), line.length());
-        LOG_PROMPT();
-    }
-    return 0;
 }
 
 int main(int argc, char const *argv[])
---
+++ SslChat/Client/handleMessages.hpp
@@ -0,0 +1,8 @@
+#ifndef HANDLEMESSAGES_HPP
+#define HANDLEMESSAGES_HPP
+
+#include <openssl/ssl.h>
+
+int handleMessages(SSL *ssl);
+
+#endif
