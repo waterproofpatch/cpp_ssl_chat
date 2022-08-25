e9160647e57ec2f62e34ac0f5fc1755066c1db9b
waterproofpatch <waterproofpatch@gmail.com>
2022-08-25 07:16:31 -0400

    new lib

--- SslChat/Ssl/ssl.hpp
+++ SslChat/Ssl/ssl.hpp
@@ -1,8 +1,14 @@
+#ifndef SSL_HPP
+#define SSL_HPP
 
 #include <openssl/ssl.h>
 
+SSL_CTX *SslLib_initCtx(void);
+void     SslLib_displayCerts(SSL *ssl);
 int      SslLib_createSocket(int port);
 SSL_CTX *SslLib_getContext();
 void     SslLib_configureContext(SSL_CTX    *ctx,
                                  const char *certPath,
                                  const char *keyPath);
+
+#endif
